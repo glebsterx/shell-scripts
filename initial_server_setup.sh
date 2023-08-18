bcfb438fa5b3a3a6ff688e27501e2286cf600bf2
Gleb <glebsterx@gmail.com>
2023-08-18 18:07:31 +0300

    Update initial_server_setup.sh

--- initial_server_setup.sh
+++ initial_server_setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 read -p "Enter username (default=$(id -u -n)): " LOGIN
 if ! test -n "$LOGIN"; then
-    LOGIN=$(id -u -n)
+    LOGIN="$(id -u -n)"
 fi
+
 read -p "Enter SMB password (default=password): " SMBPASSWORD
 if ! test -n "$SMBPASSWORD"; then
     SMBPASSWORD="password"
